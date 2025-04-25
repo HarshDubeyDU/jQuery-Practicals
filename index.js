78985658847244736e8da64f59793472fcdd4a9e
Harsh Dubey <87745474+HarshDubeyDU@users.noreply.github.com>
2025-04-25 16:09:54 +0530

    Completed all

--- index.js
+++ index.js
@@ -52,9 +52,71 @@ $(".ajax__request").click(function() {
         url: "https://jsonplaceholder.typicode.com/posts",
         success: function(data) {
             data.forEach(function(post) {
-                $("ajax").append("<h3>" + post.title + "</h3><p>" + post.body + "</p>");
+                $(".ajax").append("<h3>Title: " + post.title + "</h3><p class='post-body'>Body:<br/>" + post.body + "</p>");
             })
         }
     })
     $(".ajax__request").hide();
 });
+
+$("#identity-verification-form").submit(function(event) {
+    $("form span").hide();
+    let isValid = true;
+
+    const name = $("#name").val();
+    const email = $("#email").val();
+    const phone = $("#phone").val();
+    const password = $("#password").val();
+    const confirmPassword = $("#confirmPassword").val();
+
+    if (name === "") {
+        $("#name").after('<span class="error">Please enter your name</span>');
+        isValid = false;
+    }
+
+    if (!validateEmail(email)) {
+        $("#email").after("<span>Invalid email format</span>");
+        isValid = false;
+    }
+
+    if (!validatePhone(phone)) {
+        $("#phone").after("<span>Invalid phone format</span>");
+        isValid = false;
+    }
+
+    if (!validatePassword(password)) {
+        $("#password").after("<span>Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, and one number</span>");
+        isValid = false;
+    }
+
+    if (password !== confirmPassword) {
+        $("#confirmPassword").after("<span>Passwords do not match</span>");
+        isValid = false;
+    }
+
+    event.preventDefault();
+
+    if (isValid) {
+        const form = $("#identity-verification-form");
+        form.reset();
+
+        const formButton = $(form+"button");
+        formButton.attr("disabled", "disabled");
+        formButton.before("<span>Form is Valid.</span>");
+    }
+});
+
+function validateEmail(email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+}
+
+function validatePhone(phone) {
+    const phoneRegex = /^\d{10}$/;
+    return phoneRegex.test(phone);
+}
+
+function validatePassword(password) {
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
+    return passwordRegex.test(password);
+}
